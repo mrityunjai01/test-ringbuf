4738eeaed759aadb4b2de173d01e8c00b2df9bce
Mrityunjai Singh <mrityunjaisingh@Mrityunjais-Laptop.local>
2025-05-25 23:27:49 -0700

    add single thread benchmarks

--- benches/single_thread_ringbuf.rs
+++ benches/single_thread_ringbuf.rs
@@ -1,16 +1,104 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BatchSize::SmallInput, Criterion};
 use ringbuf::{storage::Array, traits::*, LocalRb};
 
 const BUFFER_CAP: usize = 1000;
 
-fn benchmark_single_read_empty(c: &mut Criterion) {
-    c.iter(|| {
-        let rb = LocalRb::<Array<usize, BUFFER_CAP>>::default();
-        let (mut prod, cons) = rb.split();
-        prod.try_push(1).unwrap();
-        black_box(cons.try_pop().unwrap_or_default());
+fn benchmark_single_read(c: &mut Criterion) {
+    c.bench_function("single_read_single_thread_empty_buffer", |b| {
+        b.iter_batched_ref(
+            || {
+                let rb = LocalRb::<Array<usize, BUFFER_CAP>>::default();
+                let (mut prod, cons) = rb.split();
+                prod.try_push(1).unwrap();
+                cons
+            },
+            |cons| {
+                black_box(cons.try_pop().unwrap_or_default());
+            },
+            SmallInput,
+        );
+    });
+    c.bench_function("single_read_single_thread_full_buffer", |b| {
+        b.iter_batched_ref(
+            || {
+                let rb = LocalRb::<Array<usize, BUFFER_CAP>>::default();
+                let (mut prod, cons) = rb.split();
+                for i in 0..BUFFER_CAP {
+                    prod.try_push(i).unwrap();
+                }
+                cons
+            },
+            |cons| {
+                black_box(cons.try_pop().unwrap_or_default());
+            },
+            SmallInput,
+        );
+    });
+    c.bench_function("single_read_single_thread_half_buffer", |b| {
+        b.iter_batched_ref(
+            || {
+                let rb = LocalRb::<Array<usize, BUFFER_CAP>>::default();
+                let (mut prod, cons) = rb.split();
+                for i in 0..BUFFER_CAP / 2 {
+                    prod.try_push(i).unwrap();
+                }
+                cons
+            },
+            |cons| {
+                black_box(cons.try_pop().unwrap_or_default());
+            },
+            SmallInput,
+        );
     });
 }
 
-criterion_group!(benches, benchmark_single_read_empty);
+fn benchmark_single_write(c: &mut Criterion) {
+    c.bench_function("single_write_single_thread_empty_buffer", |b| {
+        b.iter_batched_ref(
+            || {
+                let rb = LocalRb::<Array<usize, BUFFER_CAP>>::default();
+                let (prod, _cons) = rb.split();
+                prod
+            },
+            |prod| {
+                prod.try_push(black_box(1)).unwrap();
+            },
+            SmallInput,
+        );
+    });
+    c.bench_function("single_write_single_thread_full_buffer", |b| {
+        b.iter_batched_ref(
+            || {
+                let rb = LocalRb::<Array<usize, BUFFER_CAP>>::default();
+                let (mut prod, _cons) = rb.split();
+                for i in 0..BUFFER_CAP {
+                    prod.try_push(i).unwrap();
+                }
+                prod
+            },
+            |prod| {
+                prod.try_push(black_box(1)).unwrap();
+            },
+            SmallInput,
+        );
+    });
+    c.bench_function("single_write_single_thread_half_buffer", |b| {
+        b.iter_batched_ref(
+            || {
+                let rb = LocalRb::<Array<usize, BUFFER_CAP>>::default();
+                let (mut prod, _cons) = rb.split();
+                for i in 0..BUFFER_CAP / 2 {
+                    prod.try_push(i).unwrap();
+                }
+                prod
+            },
+            |prod| {
+                prod.try_push(black_box(1)).unwrap();
+            },
+            SmallInput,
+        );
+    });
+}
+
+criterion_group!(benches, benchmark_single_read, benchmark_single_write);
 criterion_main!(benches);
